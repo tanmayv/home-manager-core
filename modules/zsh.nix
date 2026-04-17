4f17f566138e6609bcb3bd9b74ac1e0dc4ecace6
Tanmay Vijayvargiya <tanmayvijay@google.com>
2026-04-17 20:21:58 +0530

    Use absolute path for Zsh dotDir to fix deprecation warning

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   programs.zsh = {
     enable = true;
+    dotDir = "${config.xdg.configHome}/zsh";
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     shellAliases = {
