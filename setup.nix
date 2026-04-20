f59c517bc2361e4b93281109e378dd8b5fb51e86
Tanmay Vijayvargiya <tanmayvijay@google.com>
2026-04-20 17:35:40 +0530

    chore: disable AI features by default for fresh clones

--- setup.nix
+++ setup.nix
@@ -4,15 +4,15 @@
   local_agent_knowledge_dir = "~/agent_knowledge";
   local_agent_knowledge_create_command = ""; # e.g., "nn"
   editor = "nvim";
-  enable-ai-workflow = true;
+  enable-ai-workflow = false;
   enable-neovim = true;
   enable-tmux-on-ssh = true;
   auto-switch-workspace-on-hgd = true;
   enable-cd-verbose = true;
   import-extras = true;
   ai_features = {
-    enable_agent_knowledge = true;
-    enable_ai_ssa_creator_skill = true;
-    enable_tmux_based_agent_comms = true;
+    enable_agent_knowledge = false;
+    enable_ai_ssa_creator_skill = false;
+    enable_tmux_based_agent_comms = false;
   };
 }
