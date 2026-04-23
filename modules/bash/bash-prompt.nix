762c287847abef04370a5a78eb13ea89c78ce4d7
Tanmay Vijayvargiya <tanmayvijay@google.com>
2026-04-23 09:35:21 +0530

    feat(bash): dynamically derive prompt colors from palette.nix using hex-to-rgb conversion

--- modules/bash/bash-prompt.nix
+++ modules/bash/bash-prompt.nix
@@ -1,4 +1,29 @@
 { pkgs, ... }:
+let
+  palette = import ../palette.nix;
+  
+  # Helper to convert hex #RRGGBB to "R;G;B" for escape sequences
+  hexToRgb = hex: 
+    let
+      # Mapping for hex to decimal
+      m = { "0"=0;"1"=1;"2"=2;"3"=3;"4"=4;"5"=5;"6"=6;"7"=7;"8"=8;"9"=9;"a"=10;"b"=11;"c"=12;"d"=13;"e"=14;"f"=15;"A"=10;"B"=11;"C"=12;"D"=13;"E"=14;"F"=15; };
+      h = builtins.substring 1 6 hex;
+      # Convert 2-digit hex to decimal
+      dec = s: (m.${builtins.substring 0 1 s} * 16) + m.${builtins.substring 1 1 s};
+      r = dec (builtins.substring 0 2 h);
+      g = dec (builtins.substring 2 2 h);
+      b = dec (builtins.substring 4 2 h);
+    in "${toString r};${toString g};${toString b}";
+
+  # Dynamically derive RGB strings from palette
+  rgb = {
+    color1 = hexToRgb palette.color1; # User
+    color2 = hexToRgb palette.color2; # CitC client
+    color3 = hexToRgb palette.color3; # Path
+    color4 = hexToRgb palette.color4; # Host
+  };
+
+in
 {
   programs.bash.initExtra = ''
     # Installs a cool, dynamic, Google-colored prompt.
@@ -36,12 +61,12 @@
       fi
     }
 
-    # Colors using tput
-    NO_COLOR="\[$(${pkgs.ncurses}/bin/tput sgr0)\]"
-    RED="\[$(${pkgs.ncurses}/bin/tput setaf 1)\]"
-    GREEN="\[$(${pkgs.ncurses}/bin/tput setaf 2)\]"
-    YELLOW="\[$(${pkgs.ncurses}/bin/tput setaf 3)\]"
-    BLUE="\[$(${pkgs.ncurses}/bin/tput setaf 4)\]"
+    # Colors using TrueColor escape sequences derived dynamically from palette.nix
+    NO_COLOR="\[\e[0m\]"
+    RED="\[\e[38;2;${rgb.color1}m\]"
+    GREEN="\[\e[38;2;${rgb.color2}m\]"
+    YELLOW="\[\e[38;2;${rgb.color3}m\]"
+    BLUE="\[\e[38;2;${rgb.color4}m\]"
 
     PS1="\
 ''${debian_chroot:+($debian_chroot)}\
