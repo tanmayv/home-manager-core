ade53162d9ceaf5c8473d3bc66c6fe89d0faac1e
Tanmay Vijayvargiya <tanmayvijay@google.com>
2026-05-08 11:50:08 +0530

    Refactor: Drop standalone support and move setup.nix
    - Removed homeConfigurations from flake.nix.
    - Removed extensions input from flake.nix.
    - Deleted setup.nix (now managed in user's deployment repo).
    
    TAG=agy
    CONV=3628b3a3-65ea-49f6-a9db-464ed3eacd60

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Minimal Home Manager configuration for Cloudtop";
+  description = "Minimal Home Manager configuration for Cloudtop (Core)";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
@@ -22,45 +22,13 @@
       url = "github:tanmayv/tasks.nvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    extensions = {
-      url = "path:/usr/local/google/home/tanmayvijay/home-manager-extensions";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs:
+  outputs = { nixpkgs, ... }@inputs:
     let
-      user = "tanmayvijay";
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      userSettings = import ./setup.nix;
     in {
-      homeConfigurations = rec {
-        cloudtop = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = { inherit inputs userSettings; };
-          modules = [ 
-            ./home.nix 
-
-            # Configure extension options
-            ({ ... }: {
-              services.agent-tracker.enable = userSettings.enable-agent-tracker or false;
-              services.agent-tracker.enableTmuxIntegration = true;
-            })
-
-            # Temporary override for local build
-            ({ pkgs, ... }: {
-              home.username = user;
-              home.homeDirectory = "/usr/local/google/home/${user}";
-            })
-          ];
-        };
-
-        # Expose the configuration under the specific user's name as well
-        "${user}" = cloudtop;
-      };
-
       homeManagerModules.default = ./home.nix;
     };
 }
