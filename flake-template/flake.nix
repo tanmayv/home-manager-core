7e2223e090bebfba146679be77798a789d4c8967
Tanmay Vijayvargiya <tanmayvijay@google.com>
2026-05-11 14:40:48 +0530

    Refactor: Revert core and template nixpkgs to stable nixos-25.11 to resolve Neovim 0.12 build bugs

--- flake-template/flake.nix
+++ flake-template/flake.nix
@@ -2,9 +2,9 @@
   description = "Example flake using minimal-cloudtop as a library";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     
